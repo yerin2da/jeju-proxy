c8a73f1bcc82f2079a05ed997dee664cd76883ad
yerin <byl06@naver.com>
2025-04-23 14:21:16 +0900

    전시 url테스트

--- jejuServer.js
+++ jejuServer.js
@@ -6,9 +6,6 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-// const apiKey = process.env.REACT_APP_API_CULTURE//문화공공데이터
-// const apiKey2 =process.env.REACT_APP_API_JEJU //비짓제주
 app.use(cors({
     origin: [
         'http://localhost:3001',                    // 로컬 개발용
