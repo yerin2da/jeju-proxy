dc2556cfa1e74438703c3793c15b49aa4a90db31
yerin <byl06@naver.com>
2025-04-28 11:08:07 +0900

    제주프록시 서버 포트 변경

--- jejuServer.js
+++ jejuServer.js
@@ -15,7 +15,7 @@ dns.setServers(['1.1.1.1', '1.0.0.1', '8.8.8.8', '8.8.4.4']);
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(cors({
     origin: [
